f5965e7d564758fe16c1a281249d3aad935e7bda
Justin Adams <AdamsJustin@protonmail.com>
2022-02-17 18:07:25 -0500

    Cleaned up usings and added comments regarding databases in Program.cs

--- WorkoutWithFriends.API/Program.cs
+++ WorkoutWithFriends.API/Program.cs
@@ -6,7 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Using program memory for now, will reset every time you restart the project
+// TODO: Switching to Sqlite in the future when hosting on Azure
 builder.Services.AddDbContext<ExerciseContext>(opt => opt.UseInMemoryDatabase("ExerciseList"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
