e4aca7de58dfb64ae845e0c177386622230b9100
Justin Adams <AdamsJustin@protonmail.com>
2022-02-20 13:44:52 -0500

    Deleted .net6 API and replaced with .net5

---
+++ backend/API/Models/ExerciseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class ExerciseContext : DbContext
+    {
+        public ExerciseContext(DbContextOptions<ExerciseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ExerciseItem> ExerciseItems { get; set; }
+    }
+}
