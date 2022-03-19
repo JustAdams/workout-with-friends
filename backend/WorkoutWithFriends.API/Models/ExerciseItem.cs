7fb32e68924957d2c5d1de28f1d1cc15d26f136f
Justin <adamsjustin@protonmail.com>
2022-03-19 13:44:29 -0400

    Added .net 6 API to be modern

---
+++ backend/WorkoutWithFriends.API/Models/ExerciseItem.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutWithFriends.API.Models
+{
+    public class ExerciseItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Reps { get; set; }
+        public int Weight { get; set; }
+        public DateTime Day { get; set; }
+        public string? Person { get; set; }
+    }
+}
---
+++ backend/WorkoutWithFriends.API/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using WorkoutWithFriends.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Using program memory for now, will reset every time you restart the project
+// TODO: Switching to Sqlite in the future when hosting on Azure
+builder.Services.AddDbContext<ExerciseContext>(opt => opt.UseInMemoryDatabase("ExerciseList"));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
